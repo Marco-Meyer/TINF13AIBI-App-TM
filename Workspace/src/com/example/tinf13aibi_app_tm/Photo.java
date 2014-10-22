1c732d740f7be0f736293dbf9b33f3caa8fe6daa
Marco Meyer <MarcoMeyer@gmx.de>
2014-10-22 16:58:38 +0200

    * added xstream libs for xml-conversion * linked those libs to the project (links are related to project location..so hopefully this will work) * dummy implementation of the photo class * basic implementation of SaveManager class. Converting a photo-object to an xml-string should work.

--- Workspace/src/com/example/tinf13aibi_app_tm/Photo.java
+++ Workspace/src/com/example/tinf13aibi_app_tm/Photo.java
@@ -1,5 +1,35 @@
 package com.example.tinf13aibi_app_tm;
 
-public class Photo {
+import java.util.Date;
 
+import android.text.format.Time;
+
+public class Photo {
+	/* 
+	 * dummy implementation for this class to test xml-writing/reading of the savemanager.
+	 * We should think about reasonable data types of "date" and "time"..
+	*/
+	private long id;
+	private String date;
+	private String time;
+	private String location;
+	
+	public Photo() {
+		this.id = 0;
+		this.date = "";
+		this.time = "";
+		this.location = "";
+	}
+	
+	public Photo(long id, String date, String time, String location) {
+		this.id = id;
+		this.date = date;
+		this.time = time;
+		this.location = location;
+	}
+
+	public long getId() {
+		return id;
+	}
+	
 }
--- Workspace/src/com/example/tinf13aibi_app_tm/SaveManager.java
+++ Workspace/src/com/example/tinf13aibi_app_tm/SaveManager.java
@@ -1,6 +1,48 @@
 package com.example.tinf13aibi_app_tm;
 
+import java.io.File;
+import java.util.Date;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.StaxDriver;
+
 public class SaveManager {
 	//fixed location we want to save our xml-files / pictures. Might a bit too inflexible..
-	private static final String directory = ""; 
+	private static final String directory = "";
+	private XStream xstream; //xstream is a lib able to convert entire classes to xml and vice versa. Convenient, isn't it?
+	
+	public SaveManager() {
+		xstream = new XStream(new StaxDriver());
+		xstream.alias("Photo", Photo.class);
+	}
+	
+	
+	//public void savePictureInDir(long pictureId, Bitmap picture) {
+		
+	//}
+	
+	public void savePictureMetaDataInXml(Photo photo) {
+		 String xml = xstream.toXML(photo);
+		 System.out.println(xml);
+		 writeXmlToFile(photo.getId(), xml);
+	}
+	
+	public Photo loadPictureMetaDataInXml(long pictureId) {
+		return (Photo) xstream.fromXML(getFileWithId(pictureId)); 
+	}
+	
+	private void writeXmlToFile(long id, String xml) {
+		//well... new File(StringToXml(xml)) maybe?..xD
+	}
+	
+	private File getFileWithId(long pictureId) {
+		// we have to search our dir and find the associated file here
+		return new File("file");
+	}
+	
+	public static void main(String[] args) {
+		Photo testphoto = new Photo(10, "today", "now", "here");
+		SaveManager sm = new SaveManager();
+		sm.savePictureMetaDataInXml(testphoto);
+	}
 }
