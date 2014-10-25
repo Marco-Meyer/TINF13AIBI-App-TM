7f9a43f392f45ef04cce2af47ee408b06a873247
Marco Meyer <MarcoMeyer@gmx.de>
2014-10-25 19:46:16 +0200

    * Added location check * added logic for devices without gps (my tablet..for example xD) * minor refactoring

--- Workspace/src/com/example/tinf13aibi_app_tm/MainActivity.java
+++ Workspace/src/com/example/tinf13aibi_app_tm/MainActivity.java
@@ -9,16 +9,21 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 	private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
 	private PhotoList list;
-	
+	private LocationManager locManager;
+	private LocationListener locListener;
+	private Location currentLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +33,27 @@ public class MainActivity extends ActionBarActivity {
         
         this.list = new PhotoList(this, (ListView)findViewById(R.id.photo_list), getDirectories());
         list.loadOldPhotoData();
+        
+        currentLocation = null;
+        locManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        setLocationListener();
     }
     
-
     @Override
+	protected void onStart() {
+		super.onStart();
+		if (locationProviderExists()) {
+			locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 3000, 0, locListener);
+		}
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		locManager.removeUpdates(locListener);
+	}
+
+	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
@@ -56,30 +78,63 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
-            if (resultCode == RESULT_OK) {
-		    	Bitmap picture = (Bitmap) data.getExtras().get("data");
-		    	 
-				
-		    	Date currentDate = new Date();
-		    	Time currentTime = new Time(currentDate.getTime()); 
-				String pictureId = new SimpleDateFormat("yyyyMMdd_HHmmss").format(currentDate);
-				
-				Location location = new Location("");
-				
-		        Photo resultPhoto = new Photo(pictureId, currentDate, currentTime,  location);
-		        list.addPhoto(resultPhoto, picture);
+            switch (resultCode) {
+            	case RESULT_OK:
+            		Bitmap picture = (Bitmap) data.getExtras().get("data");
+   		    	 
+    		    	Date currentDate = new Date();
+    		    	Time currentTime = new Time(currentDate.getTime()); 
+    				String pictureId = new SimpleDateFormat("yyyyMMdd_HHmmss").format(currentDate);
+    				
+    		        Photo resultPhoto = new Photo(pictureId, currentDate, currentTime,  currentLocation);
+    		        list.addPhoto(resultPhoto, picture);
+    		        Toast.makeText(this, "Photo added.", Toast.LENGTH_SHORT).show();
+    		        break;
+            	case RESULT_CANCELED:
+            		Toast.makeText(this, "Canceled image capture. No photo added.", Toast.LENGTH_SHORT).show();
+            		break;
+            	default:
+            		Toast.makeText(this, "Error occured. No photo added.", Toast.LENGTH_LONG).show();
             }
-       // } 	
-            // Image captured and saved to fileUri specified in the Intent
-           // } else if (resultCode == RESULT_CANCELED) {
-                // User cancelled the image capture
-           // } else {
-                // Image capture failed, advise user
-            //}
-        //}
-
-        	}
         }
+    }
+    
+    private boolean locationProviderExists() {
+    	boolean result = false;
+    	
+    	if (locManager != null) {
+    		if (locManager.getProvider(LocationManager.GPS_PROVIDER) != null) {
+    			result = true;
+    		}
+    	}
+    	
+    	return result;
+    }
+    
+    private void setLocationListener() {
+    	locListener =  new LocationListener() {
+
+			@Override
+			public void onLocationChanged(Location location) {
+				currentLocation = location; 
+			}
+
+			@Override
+			public void onStatusChanged(String provider, int status,
+					Bundle extras) {
+			}
+
+			@Override
+			public void onProviderEnabled(String provider) {
+			}
+
+			@Override
+			public void onProviderDisabled(String provider) {
+			}
+    		
+    	};
+    }
+    
     private String[] getDirectories() {
     	String photoDir = getDir("Photos", Context.MODE_PRIVATE).getAbsolutePath();
         String xmlDir = getDir("XMLs", Context.MODE_PRIVATE).getAbsolutePath();
